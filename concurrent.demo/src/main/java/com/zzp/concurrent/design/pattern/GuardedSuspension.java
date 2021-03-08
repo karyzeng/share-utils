347f27b2e45f3046259b9bbfa2c06f876d2e6089
zzp <354697171@qq.com>
2021-03-08 22:21:35 +0800

    修改代码

--- concurrent.demo/src/main/java/com/zzp/concurrent/design/pattern/GuardedSuspension.java
+++ concurrent.demo/src/main/java/com/zzp/concurrent/design/pattern/GuardedSuspension.java
@@ -1,5 +1,12 @@
 package com.zzp.concurrent.design.pattern;
 
+import com.alibaba.fastjson.JSON;
+import com.threadpool.ThreadPoolUtils;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -18,16 +25,105 @@ public class GuardedSuspension<T> {
 
     private Condition done = lock.newCondition();
 
+    private final int timeout = 1;
+
+    private final static Map<Object, GuardedSuspension> GUARDED_SUSPENSION_MAP = new ConcurrentHashMap<>();
+
+    public static <K> GuardedSuspension create(K key) {
+        GuardedSuspension guardedSuspension = new GuardedSuspension();
+        GUARDED_SUSPENSION_MAP.put(key, guardedSuspension);
+        return guardedSuspension;
+    }
+
+    public static <K, T> void fireEvent(K key, T msg) {
+        GuardedSuspension guardedSuspension = GUARDED_SUSPENSION_MAP.remove(key);
+        if (guardedSuspension != null) {
+            guardedSuspension.onChange(msg);
+        }
+    }
+
     public T get(Predicate<T> p) {
         lock.lock();
         try {
-
+            while (!p.test(obj)) {
+                done.await(1, TimeUnit.SECONDS);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             lock.unlock();
         }
-        return null;
+        return obj;
+    }
+
+    public void onChange(T obj) {
+        lock.lock();
+        try {
+            this.obj = obj;
+            done.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    static class Message {
+        private String msgId;
+        private String data;
+
+        public Message(String msgId, String data) {
+            this.msgId = msgId;
+            this.data = data;
+        }
+
+        public String getMsgId() {
+            return msgId;
+        }
+
+        public void setMsgId(String msgId) {
+            this.msgId = msgId;
+        }
+
+        public String getData() {
+            return data;
+        }
+
+        public void setData(String data) {
+            this.data = data;
+        }
+    }
+
+    static class Test {
+
+        public void sendMsg(Message msg) {
+            System.out.println(String.format("%s-发送消息：%s", Thread.currentThread().getName(), JSON.toJSONString(msg)));
+        }
+
+        public void onMessage(Message msg) {
+            GuardedSuspension.fireEvent(msg.getMsgId(), msg);
+        }
+
+        public static void main(String[] args) throws InterruptedException {
+            Test test = new Test();
+            ExecutorService es = ThreadPoolUtils.newDefaultThreadPoolExecutor();
+            String msgId = "1";
+            // 模拟请求
+            es.execute(() -> {
+                Message message = new Message(msgId, "{}");
+                test.sendMsg(message);
+                GuardedSuspension<Message> guardedSuspension = GuardedSuspension.create(msgId);
+                Message result = guardedSuspension.get(msg -> msg != null);
+                System.out.println(String.format("%s-获得返回值：%s", Thread.currentThread().getName(), JSON.toJSONString(result)));
+            });
+
+            // 休眠5秒
+            Thread.sleep(5000);
+
+            // 模拟消费
+            es.execute(() -> {
+                Message message = new Message(msgId, "{响应信息}");
+                test.onMessage(message);
+            });
+        }
     }
 
 }
