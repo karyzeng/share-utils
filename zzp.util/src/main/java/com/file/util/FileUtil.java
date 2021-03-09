b3f0e010cf70630d00436119a54890536172c236
karyzeng <354697171@qq.com>
2021-03-09 16:32:31 +0800

    新增getInputStream和encodeDownloadFileUrl方法

--- zzp.util/src/main/java/com/file/util/FileUtil.java
+++ zzp.util/src/main/java/com/file/util/FileUtil.java
@@ -1,6 +1,13 @@
 package com.file.util;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.File;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -49,6 +56,55 @@ public class FileUtil {
             return files;
         }
         return null;
+    }
+
+    /**
+     * 获取文件Url的输入流
+     * @param fileUrl
+     * @return InputStream
+     */
+    private static InputStream getInputStream(String fileUrl) {
+        try {
+            String encodeUrl = encodeDownloadFileUrl(fileUrl);
+            URL url = new URL(encodeUrl);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            // 设置连接超时间为5秒
+            conn.setConnectTimeout(5000);
+            // 设置读超时时间为5秒
+            conn.setReadTimeout(5000);
+            // 防止屏蔽程序抓取而返回403错误
+            conn.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
+            // 得到输入流
+            InputStream inputStream = conn.getInputStream();
+            return inputStream;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 对下载文件的url进行编码，主要是处理下载的文件包含有中文字符的情况
+     * @param fileUrl
+     * @return String
+     */
+    private static String encodeDownloadFileUrl(String fileUrl) {
+        if (StringUtils.isBlank(fileUrl)) {
+            return null;
+        }
+        int fileNameIndex = fileUrl.lastIndexOf("/");
+        String fileName = fileUrl.substring(fileNameIndex + 1);
+        String urlPrefix = fileUrl.substring(0, fileNameIndex + 1);
+        String encodeFileName = null;
+        try {
+            encodeFileName = URLEncoder.encode(fileName, "utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        if (StringUtils.isNotBlank(encodeFileName)) {
+            return urlPrefix + encodeFileName;
+        }
+        return fileUrl;
     }
 
 }
