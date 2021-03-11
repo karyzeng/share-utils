0d9404b9cfb54d8e2c371de538fbb66a61843a11
zzp <354697171@qq.com>
2021-03-11 21:41:14 +0800

    限流器

---
+++ concurrent.demo/src/main/java/com/zzp/concurrent/analysis/RateLimiter.java
@@ -0,0 +1,15 @@
+package com.zzp.concurrent.analysis;
+
+/**
+ * @Description 限流器
+ * @Author Garyzeng
+ * @since 2021.03.11
+ **/
+public class RateLimiter {
+
+    /**
+     * 桶当前容量
+     */
+    private int storedPermits = 0;
+
+}
