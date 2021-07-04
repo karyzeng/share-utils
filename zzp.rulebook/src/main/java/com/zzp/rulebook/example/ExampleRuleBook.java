7ff90b7e8774905b29f7393722dc2a672d728283
zzp <354697171@qq.com>
2021-07-04 16:15:16 +0800

    feat: rulebook例子

---
+++ zzp.rulebook/src/main/java/com/zzp/rulebook/example/ExampleRuleBook.java
@@ -0,0 +1,19 @@
+package com.zzp.rulebook.example;
+
+import com.deliveredtechnologies.rulebook.model.Rule;
+import com.deliveredtechnologies.rulebook.model.RuleBook;
+import com.deliveredtechnologies.rulebook.model.rulechain.cor.CoRRuleBook;
+
+/**
+ * @Description rulebook例子
+ * @Author Garyzeng
+ * @since 2021.07.04
+ **/
+public class ExampleRuleBook extends CoRRuleBook<Double> {
+
+    @Override
+    public void defineRules() {
+
+    }
+
+}
