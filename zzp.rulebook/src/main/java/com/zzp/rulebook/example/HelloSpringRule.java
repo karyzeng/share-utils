9055d8a40a5ddeb4bf3ef19f532327abfd726b4c
zzp <354697171@qq.com>
2021-07-04 16:15:29 +0800

    feat: HelloSpringRule

---
+++ zzp.rulebook/src/main/java/com/zzp/rulebook/example/HelloSpringRule.java
@@ -0,0 +1,31 @@
+package com.zzp.rulebook.example;
+
+import com.deliveredtechnologies.rulebook.annotation.*;
+import com.deliveredtechnologies.rulebook.spring.RuleBean;
+
+/**
+ * @Description HelloSpringRule
+ * @Author Garyzeng
+ * @since 2021.07.04
+ **/
+@RuleBean
+@Rule(order = 1)
+public class HelloSpringRule {
+
+    @Given("hello")
+    private String hello;
+
+    @Result
+    private String result;
+
+    @When
+    public boolean when() {
+        return hello != null;
+    }
+
+    @Then
+    public void then() {
+        result = hello;
+    }
+
+}
