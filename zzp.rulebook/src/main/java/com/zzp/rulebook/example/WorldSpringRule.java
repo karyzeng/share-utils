23cdaa95b80d37234223e16f3bdf7702b133c8c4
zzp <354697171@qq.com>
2021-07-04 16:15:43 +0800

    feat: WorldSpringRule

---
+++ zzp.rulebook/src/main/java/com/zzp/rulebook/example/WorldSpringRule.java
@@ -0,0 +1,36 @@
+package com.zzp.rulebook.example;
+
+import com.deliveredtechnologies.rulebook.annotation.*;
+import com.deliveredtechnologies.rulebook.spring.RuleBean;
+import com.zzp.rulebook.service.RulebookService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @Description WorldSpringRule
+ * @Author Garyzeng
+ * @since 2021.07.04
+ **/
+@RuleBean
+@Rule(order = 2)
+public class WorldSpringRule {
+
+    @Autowired
+    private RulebookService rulebookService;
+
+    @Given("world")
+    private String world;
+
+    @Result
+    private String result;
+
+    @When
+    public boolean when() {
+        return world != null;
+    }
+
+    @Then
+    public void then() {
+        result = rulebookService.test(result, world);
+    }
+
+}
