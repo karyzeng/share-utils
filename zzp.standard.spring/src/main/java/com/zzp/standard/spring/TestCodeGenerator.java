ffdc13cf315ea42765c7a492182c5b6ca9e7aa28
karyzeng <354697171@qq.com>
2021-07-23 18:50:35 +0800

    feat: 添加TestCodeGenerator

---
+++ zzp.standard.spring/src/main/java/com/zzp/standard/spring/TestCodeGenerator.java
@@ -0,0 +1,18 @@
+package com.zzp.standard.spring;
+
+import com.zzp.code.generator.ZzpAutoGenerator;
+
+/**
+ * @Description TODO
+ * @Author karyzeng
+ * @since 2021.07.23
+ **/
+public class TestCodeGenerator {
+
+    public static void main(String[] args) {
+        ZzpAutoGenerator autoGenerator = new ZzpAutoGenerator();
+        autoGenerator.general();
+    }
+
+
+}
